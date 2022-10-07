7811124891eaed194fc5b49122eb489f9b15c2a7
tomo071 <99002249+tomo071@users.noreply.github.com>
2022-10-07 17:50:06 +0900

    [start]

---
+++ study.java
@@ -0,0 +1,17 @@
+public class Main {
+  public static void main(String[] args) {
+    status("国語",70);
+    status("算数",60);
+    status("理科",40);
+  }
+  public static void status(String subject,int score) {
+    if (score >= 50 ){
+      System.out.println(subject +"の点数は"+score+"です")
+      System.out.println("平均点以下です")
+    }
+    else{
+      System.out.println(subject +"の点数は"+score+"です")
+      System.out.println("平均点以上です")
+    }
+  }
+}
