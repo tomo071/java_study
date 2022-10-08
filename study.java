9dd6f0239c0c64caf12f43f5c137bda11ebdbee0
tomo071 <99002249+tomo071@users.noreply.github.com>
2022-10-08 16:35:02 +0900

    [add]コンパイル実行

--- study.java
+++ study.java
@@ -1,4 +1,4 @@
-public class Main {
+class Main {
   public static void main(String[] args) {
     status("国語",70);
     status("算数",60);
@@ -6,12 +6,12 @@ public class Main {
   }
   public static void status(String subject,int score) {
     if (score >= 50 ){
-      System.out.println(subject +"の点数は"+score+"です")
-      System.out.println("平均点以下です")
+      System.out.println(subject +"の点数は"+score+"です");
+      System.out.println("平均点以下です");
     }
     else{
-      System.out.println(subject +"の点数は"+score+"です")
-      System.out.println("平均点以上です")
+      System.out.println(subject +"の点数は"+score+"です");
+      System.out.println("平均点以上です");
     }
   }
 }
