c8e7d4008c9746187610aebe8b81e9f57610877a
tomo071 <99002249+tomo071@users.noreply.github.com>
2022-10-09 12:10:21 +0900

    [update]scanner使用

--- study.java
+++ study.java
@@ -1,17 +1,12 @@
+import java.util.Scanner;
+
 class Main {
   public static void main(String[] args) {
-    status("国語",70);
-    status("算数",60);
-    status("理科",40);
-  }
-  public static void status(String subject,int score) {
-    if (score >= 50 ){
-      System.out.println(subject +"の点数は"+score+"です");
-      System.out.println("平均点以下です");
-    }
-    else{
-      System.out.println(subject +"の点数は"+score+"です");
-      System.out.println("平均点以上です");
-    }
+    Scanner scanner = new Scanner(System.in);
+    
+    System.out.print("名前：");
+    String firstName = scanner.next();
+    
+    System.out.println("名前は" + firstName + "です");
   }
 }
