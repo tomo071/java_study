f4eed34dcbeeeb2de4a2de8d245513f21e003c5c
tomo071 <99002249+tomo071@users.noreply.github.com>
2022-10-11 15:57:35 +0900

    [update]戻り値

--- study.java
+++ study.java
@@ -4,9 +4,25 @@ class Main {
   public static void main(String[] args) {
     Scanner scanner = new Scanner(System.in);
     
+    System.out.print("苗字：");
+    String fName = scanner.next();
     System.out.print("名前：");
-    String firstName = scanner.next();
+    String lName = scanner.next();
+    System.out.print("年齢は：");
     
-    System.out.println("名前は" + firstName + "です");
+    String name = fullName(fName, lName);
+    
+    printData(name, 25);
+    
   }
+
+  public static void printData(String name, int age) {
+    System.out.println("私の名前は" + name + "です");
+    System.out.println("年齢は" + age + "歳です");
+  }
+
+  public static String fullName(String firstName,String lastName){
+    return firstName + " " + lastName;
+  }
+  
 }
