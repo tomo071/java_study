e223e94c4680dae10f679146e0dc36e0c96a64b9
tomo071 <99002249+tomo071@users.noreply.github.com>
2022-10-12 13:09:13 +0900

    [add]bmi

--- study.java
+++ study.java
@@ -8,21 +8,24 @@ class Main {
     String fName = scanner.next();
     System.out.print("名前：");
     String lName = scanner.next();
-    System.out.print("年齢は：");
-    
+
     String name = fullName(fName, lName);
     
-    printData(name, 25);
+    printData(name, 25, bmi(1.65,65.0));
     
   }
 
-  public static void printData(String name, int age) {
+  public static void printData(String name, int age, double bmi) {
     System.out.println("私の名前は" + name + "です");
     System.out.println("年齢は" + age + "歳です");
+    System.out.println("bmiは" + bmi + "です");
   }
 
   public static String fullName(String firstName,String lastName){
     return firstName + " " + lastName;
   }
   
+  public static double bmi(double height,double weight){
+    return weight / height / height;
+  }
 }
