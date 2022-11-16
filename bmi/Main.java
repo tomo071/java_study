1e2837950d246bba17731926abaa463a949a480a
tomo071 <99002249+tomo071@users.noreply.github.com>
2022-11-16 18:44:03 +0900

    [update]present追加

--- bmi/Main.java
+++ bmi/Main.java
@@ -4,6 +4,7 @@ class Main {
   public static void main(String[] args) {
     Scanner scanner = new Scanner(System.in);
     
+      System.out.print("-----過去-----");
       System.out.print("名前：");
       String firstName = scanner.next();
       System.out.print("名字：");
@@ -23,6 +24,20 @@ class Main {
       past.setWeight(weight);
       past.setAge(age);
       past.Bmi();
+
+      System.out.print("-----現在-----");
+      System.out.print("身長(m)：");
+      double nowHeight = scanner.nextDouble();
+      System.out.print("体重(kg)：");
+      double nowWeight = scanner.nextDouble();
+
+      Present present = new Present();
+      present.setFname(firstName);
+      present.setLname(lastName);
+      present.setHeight(nowHeight);
+      present.setWeight(nowWeight);
+      present.setAge(age);
+      present.Bmi();
     
   }
 }
