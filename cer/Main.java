a32a00d5f95c4b488fcd3fc5178c99007782dbfd
tomo071 <99002249+tomo071@users.noreply.github.com>
2022-11-18 17:32:25 +0900

    [add]

--- cer/Main.java
+++ cer/Main.java
@@ -1,9 +1,23 @@
-import java.util.Scanner;
 
-class Main {
+class Main {  
   public static void main(String[] args) {
-    Scanner scanner = new Scanner(System.in);
+    Car car = new Car();
+
+    car.setName("フェラーリ");
     
+    car.setColor("赤");
     
+    Bicycle bicycle = new Bicycle();
+
+    bicycle.setName("ビアンキ");
+    
+    bicycle.setColor("緑");
+    
+    System.out.println("【車の情報】");
+    car.printData();
+
+    System.out.println("=================");
+    System.out.println("【自転車の情報】");
+    bicycle.printData();
   }
 }
