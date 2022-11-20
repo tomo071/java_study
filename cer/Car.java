9e9cb5b2cb30fdb30ca130b7b06fb6f1c45ab570
tomo071 <99002249+tomo071@users.noreply.github.com>
2022-11-20 21:14:33 +0900

    [update]サブクラスのカスタマイズ

--- cer/Car.java
+++ cer/Car.java
@@ -1,3 +1,20 @@
 class Car extends Vehicle{
-  
+  private int fuel = 50;
+
+  public int getFuel(){
+    return this.fuel;
+  }
+
+  public void charge(int litre) {
+    System.out.println(litre + "L給油します");
+    if (litre <= 0) {
+      System.out.println("給油できません");
+    } else if (litre + this.fuel >= 100) {
+      System.out.println("満タンまで給油します");
+      this.fuel = 100;
+    } else {
+      this.fuel += litre;
+    }
+    System.out.println("ガソリン量：" + this.fuel + "L");
+  }
 }
